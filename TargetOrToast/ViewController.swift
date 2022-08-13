f8b45256e083c7a10d4064fc5131808cf4f1bb99
fdevinar <fc_devinar@hotmail.com>
2022-08-13 16:26:12 -0300

    Initial Commit

---
+++ TargetOrToast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TargetOrToast
+//
+//  Created by Fabricio Devinar on 13/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
