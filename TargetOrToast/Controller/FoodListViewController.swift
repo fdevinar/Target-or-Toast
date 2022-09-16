9e97878a2dbdc7fd7cbbb615f0f74478e41b30e7
fdevinar <fc_devinar@hotmail.com>
2022-09-16 19:20:41 -0300

    table view food item delegate methods begin

--- TargetOrToast/Controller/FoodListViewController.swift
+++ TargetOrToast/Controller/FoodListViewController.swift
@@ -7,27 +7,25 @@
 
 import UIKit
 
+//MARK: - UIView Controller
 class FoodListViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-    
+
     var foodItems: [FoodItem] = [
         FoodItem(name: "Chicken", unit: "100g", protein: 30, carbs: 0, fats: 2, cals: 160),
         FoodItem(name: "Rice", unit: "100g", protein: 2, carbs: 30, fats: 2, cals: 180),
         FoodItem(name: "Olive Oil", unit: "spoon", protein: 0, carbs: 0, fats: 12, cals: 100),
         FoodItem(name: "Eggs", unit: "unit", protein: 6, carbs: 0, fats: 5, cals: 80),
-        FoodItem(name: "Whey", unit: "dosage", protein: 23, carbs: 15, fats: 10, cals: 240),
+        FoodItem(name: "Whey", unit: "2 scoops", protein: 23, carbs: 15, fats: 10, cals: 240),
         FoodItem(name: "Steak", unit: "100g", protein: 30, carbs: 5, fats: 15, cals: 280),
         FoodItem(name: "Nuts", unit: "100g", protein: 8, carbs: 4, fats: 15, cals: 300)
     ]
-    
-    
-    //var foodItems:Array = ["test","xyz","lorem","ipsum"]
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
         tableView.dataSource = self
         tableView.register(UINib(nibName: "FoodItemCell", bundle: nil), forCellReuseIdentifier: "ReusableCell")
 
@@ -35,12 +33,12 @@ class FoodListViewController: UIViewController {
     }
 }
 
+//MARK: - TableView Data Source
 extension FoodListViewController: UITableViewDataSource {
     // gets number of rows
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return foodItems.count
     }
-    
     // provides cell for each row
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let idx = indexPath.row
@@ -50,9 +48,6 @@ extension FoodListViewController: UITableViewDataSource {
         var cellColor = UIColor(named: "white")
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath) as! FoodItemCell
-        
-        
-        
         switch highMacro {
             case "protein":
                 cellColor = .red
@@ -67,12 +62,17 @@ extension FoodListViewController: UITableViewDataSource {
         cell.contentCell.backgroundColor = cellColor?.withAlphaComponent(0.4)
         cell.name.text = name
         cell.unit.text = "(" + unit + ")"
-
-        
         return cell
-        
     }
-    
-    
 }
 
+//MARK: - TableView Delegate Methods
+
+extension FoodListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let idx = indexPath.row
+        print(idx)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
